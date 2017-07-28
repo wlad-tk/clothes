a251837e81539f7e0e15e1862e24635931f91c5f
wlad-tk <vlad.tkachenko@smoozeedev.com>
2017-07-28 23:07:10 +0300

    add chunks

--- src/index.js
+++ src/index.js
@@ -3,22 +3,36 @@ import React from "react";
 import {browserHistory, Router, Route, IndexRoute} from "react-router";
 
 import RootBlock from './components/root_block';
-import Home from './components/content/Home';
-import Gallery from './components/content/Gallery';
-import Tailoring from './components/content/Tailoring';
-import Printing from './components/content/Printing';
-import Contact from './components/content/Contact';
 
 import './style/css/style.less'
+
+
+const errorLoading = (error) => {
+    throw new Error(`Dynamic page loading failed: ${error}`);
+};
+
+const loadRoute = (cb) => {
+    return module => cb(null, module.default);
+};
 
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/" component={RootBlock}>
-            <IndexRoute component={Home}/>
-            <Route path='/gallery' component={Gallery} />
-            <Route path='/tailoring' component={Tailoring} />
-            <Route path='/print' component={Printing} />
-            <Route path='/contact' component={Contact} />
+            <IndexRoute
+                getComponent={(location, cb) => {System.import('./components/content/Home').then(loadRoute(cb)).catch(errorLoading);}}
+            />
+            <Route path='/gallery'
+                   getComponent={(location, cb) => {System.import('./components/content/Gallery').then(loadRoute(cb)).catch(errorLoading);}}
+            />
+            <Route path='/tailoring'
+                   getComponent={(location, cb) => {System.import('./components/content/Tailoring').then(loadRoute(cb)).catch(errorLoading);}}
+            />
+            <Route path='/print'
+                   getComponent={(location, cb) => {System.import('./components/content/Printing').then(loadRoute(cb)).catch(errorLoading);}}
+            />
+            <Route path='/contact'
+                   getComponent={(location, cb) => {System.import('./components/content/Contact').then(loadRoute(cb)).catch(errorLoading);}}
+            />
         </Route>
     </Router>,
     document.getElementById('first')
